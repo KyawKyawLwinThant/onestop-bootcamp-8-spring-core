8eddb482f1d8a3354f44357b2b6aef238e06780b
Kyaw Kyaw Lwin <kyawkyawlwinthant@gmail.com>
2024-03-20 15:48:57 +0630

    lazy bean

--- di-switch-beans/src/main/java/org/example/service/ReportService.java
+++ di-switch-beans/src/main/java/org/example/service/ReportService.java
@@ -5,12 +5,17 @@ import org.example.ds.Report;
 import org.example.formatter.ReportFormatter;
 import org.example.provider.ReportProvider;
 import org.example.repository.ReportRepository;
+import org.ocpsoft.prettytime.PrettyTime;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
 //i18n internationalization
-@Service
+@Service @Scope("prototype")
 public class ReportService {
+    @Autowired
+    private PrettyTime prettyTime;
 
     private final ReportFormatter reportFormatter;
     private final ReportProvider reportProvider;
